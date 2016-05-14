6da2f4cb496e70eeeeaf1b8f34d1b15af33c7f34
John P <247saferock@gmail.com>
2016-05-14 00:52:37 -0700

    psuedo

---
+++ psuedo-code.rb
@@ -0,0 +1,26 @@
+1. #sum of 2 integers psuedo-code
+get input for first number
+set answer as variable1
+get input for second number
+set answer as variable2
+add result variable to capture variable1 add with variable2 together
+print result
+
+2. # method that takes an array of strings, and returns a string that is all those strings concatenated together
+create method for string array argument
+set first string to first element in argument string
+start an iternation for each string in that string argument
+add new iternation to first string element
+flatten the string 
+print resulted string
+end
+
+3. #method that takes an array of integers, and returns a new array with every other element
+Create array of integers
+create method for integer arguments
+set new empty integer array
+start an iteration with index values on the integer elements
+if index is even then save element into new integer array
+if index is odd then do not print and skip
+print new array
+
